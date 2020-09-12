0f395a42c7ff82c3c541e471580a052b8aec9cc4
Shianne Schrum <shianneschrum14@gmail.com>
2020-09-11 22:54:55 -0400

    Added Project files

---
+++ Collex_Penguin1/Assets/Scripts/PlayBowAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBowAnim : MonoBehaviour
+{
+    Animator animator;
+    public float time = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Invoke("PlayAnimation", time);
+
+    }
+
+    void PlayAnimation()
+    {
+        animator.SetTrigger("PlayBow");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
